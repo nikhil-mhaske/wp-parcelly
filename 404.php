eaf302a07db1a2b3defde389f16bc33d2a74a28a
wisdm-nikhil <nikhil.mhaske@wisdmlabs.com>
2023-03-27 16:16:42 +0530

    404 Page added

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+?>
+
+
+<div class="error-display">
+    <h1>404</h1>
+    <h4>the page you are looking for not found!</h4>
+    <p>Looks Like Your Are Lost</p>
+</div>
+
+<?php get_footer(); ?>
