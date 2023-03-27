3240a1f5765aeba7a2b54b66772d3efaeae82a7c
wisdm-nikhil <nikhil.mhaske@wisdmlabs.com>
2023-03-27 16:11:37 +0530

    Added posts loop on index.php

--- index.php
+++ index.php
@@ -2,7 +2,22 @@
 get_header();
 ?>
 
-    
 
+<div class="post-display">
+    <h1>Check out our latest Posts</h1>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    <?php get_footer(); ?>
+            <h2><?php the_title(); ?></h2>
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+
+        <?php endwhile;
+    else : ?>
+
+        <p><?php _e('Sorry, no posts found.'); ?></p>
+
+    <?php endif; ?>
+</div>
+
+<?php get_footer(); ?>
